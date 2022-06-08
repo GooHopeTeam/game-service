27d724803fc11a8ae926904d169f9d81de85be5b
ignatyevm <polyndromdev@gmail.com>
2022-06-09 06:15:52 +1000

    add @Repository annotaion

--- src/main/java/ru/goohope/gameservice/repository/PublisherRepository.java
+++ src/main/java/ru/goohope/gameservice/repository/PublisherRepository.java
@@ -1,8 +1,10 @@
 package ru.goohope.gameservice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.goohope.gameservice.model.Publisher;
 
+@Repository
 public interface PublisherRepository extends JpaRepository<Publisher, Long> {
 
 }
--- src/main/java/ru/goohope/gameservice/repository/TagRepository.java
+++ src/main/java/ru/goohope/gameservice/repository/TagRepository.java
@@ -1,8 +1,10 @@
 package ru.goohope.gameservice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.goohope.gameservice.model.Tag;
 
+@Repository
 public interface TagRepository extends JpaRepository<Tag, Long> {
 
 }
