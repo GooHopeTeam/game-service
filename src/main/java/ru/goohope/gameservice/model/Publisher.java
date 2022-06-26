8cb4d66344986e47ff830cbadbb7d5aa8ccf2e64
ignatyevm <polyndromdev@gmail.com>
2022-06-27 07:18:40 +1000

    add description and logo_url to Publisher model

--- src/main/java/ru/goohope/gameservice/model/Publisher.java
+++ src/main/java/ru/goohope/gameservice/model/Publisher.java
@@ -6,18 +6,35 @@ import javax.persistence.*;
 import java.util.List;
 
 @Entity
-@Table(name = "publisher")
+@Table(name = "publisher", uniqueConstraints = @UniqueConstraint(name = "unique_name", columnNames = {"name"}))
 public class Publisher {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(columnDefinition = "serial")
     Long id;
 
     @Column(name = "name", unique = true, nullable = false, length = 512)
     String name;
 
+    @Column(name = "description", nullable = false, columnDefinition = "text")
+    String description;
+
+    @Column(name = "logo_url", nullable = false)
+    String logoUrl;
+
     @OneToMany(mappedBy = "publisher", fetch = FetchType.LAZY)
     List<Game> games;
+
+    public Publisher() {
+
+    }
+
+    public Publisher(String name, String description, String logoUrl) {
+        this.name = name;
+        this.description = description;
+        this.logoUrl = logoUrl;
+    }
 
     public Long getId() {
         return id;
